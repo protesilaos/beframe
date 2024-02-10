e911ab388d01aba00efc0ed7148f4e955c46f866
Protesilaos Stavrou <info@protesilaos.com>
2024-02-10 07:19:26 +0200

    Bump version to trigger GNU ELPA package rebuild

--- beframe.el
+++ beframe.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/beframe
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Package-Requires: ((emacs "28.1"))
 
 ;; This file is NOT part of GNU Emacs.
