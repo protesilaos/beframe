8c8a6bea14ec886b176a9229064e5cae76722315
Protesilaos Stavrou <info@protesilaos.com>
2026-02-04 16:58:25 +0200

    Update the copyright years in beframe-transient.el

--- beframe-transient.el
+++ beframe-transient.el
@@ -1,6 +1,6 @@
 ;;; beframe-transient.el --- Transient user interface for Beframe -*- lexical-binding: t -*-
 
-;; Copyright (C) 2025  Free Software Foundation, Inc.
+;; Copyright (C) 2025-2026  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
